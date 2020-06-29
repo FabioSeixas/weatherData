8d48b4a856a03b15cc57145a4d55ee0ffc23e78f
Fábio <flscosta94@gmail.com>
2020-06-29 13:59:51 -0600

    start a project

---
+++ R/clean_automatica.R
@@ -0,0 +1,66 @@
+#' Title
+#' @import dplyr
+#' @import tidyr
+#' @import readxl
+#' @export
+clean_automatic = function(directory){
+
+  for(data_file in dir(directory)){
+    process_xls_data(paste0(directory, "\\",
+                        data_file)) -> data
+
+    final_data = tryCatch({
+      left_join(data,
+                final_data,
+                by = "Data")
+    },
+    error = function(cond){
+      final_data = data
+      return(final_data)
+    },
+    finally = {
+      message(paste("O arquivo:", data_file,
+                    "terminou de ser processado"))
+    })
+  }
+
+  return(final_data)
+}
+
+#' @export
+process_xls_data = function(file){
+
+  data = read_xls(file,
+                skip = 9)
+
+  data_inicio = read_xls(file,
+                         skip = 10,
+                         n_max = 1) %>%
+    .[[1, 1]]
+
+  message(paste("Data de início dos dados:",
+              format(data_inicio, "%d de %B de %Y")))
+
+  origin = as.Date(data_inicio) - as.integer(data[[2, 1]])
+
+  # Ajustes
+  data = data[-1,]
+  names(data)[1] = "Data"
+
+  # Variáveis
+  vars = vars_list_procedure(data)
+
+  # Extrair variáveis
+  new_data = tibble(Data = data$Data) %>%
+    mutate(Data = as.Date(as.integer(Data), origin = origin))
+
+  for(variable in vars){
+    message(paste("Adicionando", variable, "a tabela"))
+    new_data[variable] = extrair_var(data, variable, origin)
+  }
+
+  # Colocar nomes das colunas
+  names(new_data) = c("Data", names(vars))
+
+  return(new_data)
+}
---
+++ R/vars_list_procedure.R
@@ -0,0 +1,66 @@
+#' Title
+#' @import here
+#' @import stringr
+#' @import dplyr
+#' @import tidyr
+#' @export
+vars_list_procedure = function(data){
+
+  # Variáveis
+  vars_list = vars_list
+
+  var_vec = c()
+
+  for(i in 1:(ncol(data))){
+
+    str_remove_all(names(data)[i],
+                   pattern = fixed("...")) %>%
+      str_remove_all(pattern = fixed("(%)")) %>%
+      str_remove_all(pattern = fixed("(°C)")) %>%
+      str_remove_all(pattern = fixed("(hPa)")) %>%
+      str_remove_all(pattern = fixed("(m/s)")) %>%
+      str_remove_all(pattern = fixed("(graus)")) %>%
+      str_remove_all(pattern = fixed("(graus)")) %>%
+      str_remove_all(pattern = fixed("(mm)")) %>%
+      str_remove_all(pattern = fixed("(KJ/M²)")) %>%
+      str_remove_all(pattern = fixed(i)) %>%
+      str_trim() -> var_vec[i]
+  }
+
+  var_vec = as.factor(var_vec)
+
+  for(i in var_vec %>% levels){
+    if(str_count(i) == 0){
+      var_vec = droplevels(var_vec, i)
+    }
+  }
+
+
+  ##############################
+  dic = c()
+
+  for(i in 1:(var_vec %>%
+              levels %>%
+              length)){
+    for(n in 1:(vars_list %>%
+                length)){
+      if((levels(var_vec)[i]) %in% (vars_list[[n]])){
+
+        message(paste("Encontrado: ", levels(var_vec)[i],
+                    " e ", names(vars_list)[n]))
+
+        key = names(vars_list)[n]
+
+        value = levels(var_vec)[i]
+
+        names(value) = key
+
+        dic = c(dic, value)
+
+      }
+    }
+  }
+
+  return(dic)
+
+}
